357f3bea848c37498dcfc126f632ac0b5146554e
Unlimited Yusuf <91716920+limitedYusuf@users.noreply.github.com>
2024-08-12 00:29:32 +0800

    try: vercel

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const puppeteer = require('puppeteer');
 const cors = require('cors');
 const app = express();
-const port = 3000;
+const port = 6666;
 
 app.use(cors());
 app.use(express.json());
